3738d93b0eea29d855107e932a8a1caecbeb0237
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-03-12 14:28:23 +0530

    Update models.py

--- BindClassification/models.py
+++ BindClassification/models.py
@@ -137,3 +137,81 @@ class SE3Transformer(nn.Module):
             h = layer(h)
 
         return h
+
+
+class Dual_SE3Transformer(nn.Module):
+    """SE(3) equivariant GCN with attention"""
+    def __init__(self, num_layers: int, atom_feature_size: int, 
+                 num_channels: int, num_nlayers: int=1, num_degrees: int=4, 
+                 edge_dim: int=4, div: float=4, pooling: str='avg', n_heads: int=1, **kwargs):
+        super().__init__()
+        # Build the network
+        self.num_layers = num_layers
+        self.num_nlayers = num_nlayers
+        self.num_channels = num_channels
+        self.num_degrees = num_degrees
+        self.edge_dim = edge_dim
+        self.div = div
+        self.pooling = pooling
+        self.n_heads = n_heads
+
+        self.fibers = {'in': Fiber(1, atom_feature_size),
+                       'mid': Fiber(num_degrees, self.num_channels),
+                       'out': Fiber(1, num_degrees*self.num_channels)}
+
+        blocks_AB = self._build_gcn(self.fibers)
+        blocks_AG = self._build_gcn(self.fibers)
+        
+        self.Gblock_AB, self.FCblock_AB = blocks_AB
+        self.Gblock_AG, self.FCblock_AG = blocks_AG
+
+        self.combine_FCblock = []
+        self.combine_FCblock.append(nn.Linear(self.fibers['out'].n_features, int(self.fibers['out'].n_features/2)))
+        self.combine_FCblock.append(nn.ReLU(inplace=True))
+        self.combine_FCblock.append(nn.Linear(int(self.fibers['out'].n_features/2), 1))
+
+    def _build_gcn(self, fibers):
+        # Equivariant layers
+        Gblock = []
+        fin = fibers['in']
+        for i in range(self.num_layers):
+            Gblock.append(GSE3Res(fin, fibers['mid'], edge_dim=self.edge_dim, 
+                                  div=self.div, n_heads=self.n_heads))
+            Gblock.append(GNormSE3(fibers['mid']))
+            fin = fibers['mid']
+        Gblock.append(GConvSE3(fibers['mid'], fibers['out'], self_interaction=True, edge_dim=self.edge_dim))
+
+        # Pooling
+        if self.pooling == 'avg':
+            Gblock.append(GAvgPooling())
+        elif self.pooling == 'max':
+            Gblock.append(GMaxPooling())
+
+        # FC layers
+        FCblock = []
+        FCblock.append(nn.Linear(self.fibers['out'].n_features, self.fibers['out'].n_features))
+        FCblock.append(nn.ReLU(inplace=True))
+#         FCblock.append(nn.Linear(self.fibers['out'].n_features, out_dim))
+
+        return nn.ModuleList(Gblock), nn.ModuleList(FCblock)
+
+    def forward(self, G_AB, G_AG):
+        # Compute equivariant weight basis from relative positions
+        (basis_AB, r_AB), (basis_AG, r_AG) = get_basis_and_r(G_AB, self.num_degrees-1),\
+                                             get_basis_and_r(G_AG, self.num_degrees-1)
+
+        # encoder (equivariant layers)
+        h_AB, h_AG = {'0': G_AB.ndata['f']}, {'0': G_AG.ndata['f']}
+        for i in range(len(self.Gblock_AB)):
+            h_AB, h_AG = self.Gblock_AB[i](h_AB, G=G_AB, r=r_AB, basis=basis_AB),\
+                         self.Gblock_AG[i](h_AG, G=G_AG, r=r_AG, basis=basis_AG)
+
+        for i in range(len(self.FCblock_AB)):
+            h_AB, h_AG = self.FCblock_AB[i](h_AB), self.FCblock_AG[i](h_AG)
+
+        h = torch.cat((h_AB, h_AG), 1)
+        
+        for layer in self.combine_FCblock:
+            h = layer(h)
+            
+        return h

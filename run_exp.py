7fc5c694cf2a69ec3e65ed13ebe6f991f882abf8
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2022-04-01 10:10:44 +0530

    Update run_exp.py

--- run_exp.py
+++ run_exp.py
@@ -15,6 +15,7 @@ from torch_geometric.data import Data, DataLoader
 from models import *
 from loss import *
 
+
 def train(encoder_model, contrast_model, dataloader, optimizer, topk=1):
     encoder_model.train()
     epoch_loss = 0
@@ -58,9 +59,6 @@ def test(encoder_model, dataloader):
 
 
 def main():
-    path = "mydata.json"
-    batch_size = 128
-    topk = (1,)
     
     data_lst = prepare_data(path, batch_size)
     dataloader = DataLoader(data_lst, batch_size=batch_size)
@@ -72,15 +70,15 @@ def main():
                            A.NodeDropping(pn=0.1),
                            A.FeatureMasking(pf=0.1),
                            A.EdgeRemoving(pe=0.1)], 1)
-    gconv = GConv(node_dim=node_dim, edge_dim=edge_dim, hidden_dim=32, num_layers=5).to(device)
-    encoder_model = Encoder(encoder=gconv, augmentor=(aug1, aug2)).to(device)
-    contrast_model = DualBranchContrast(loss=InfoNCE(tau=0.2), mode='G2G').to(device)
+    gconv = GConv(node_dim=node_dim, edge_dim=edge_dim, hidden_dim=args.hidden_dim, num_layers=args.num_layers).to(args.device)
+    encoder_model = Encoder(encoder=gconv, augmentor=(aug1, aug2)).to(args.device)
+    contrast_model = DualBranchContrast(loss=InfoNCE(tau=temperature), mode='G2G').to(args.device)
 
-    optimizer = Adam(encoder_model.parameters(), lr=0.01)
+    optimizer = Adam(encoder_model.parameters(), lr=lr)
 
     with tqdm(total=100, desc='(T)') as pbar:
-        for epoch in range(1, 101):
-            loss = train(encoder_model, contrast_model, dataloader, optimizer, topk)
+        for epoch in range(args.epochs):
+            loss = train(encoder_model, contrast_model, dataloader, optimizer, args.topk)
             pbar.set_postfix({'loss': loss})
             pbar.update()
         print(f"loss = {loss}")
@@ -90,5 +88,19 @@ def main():
 
 
 if __name__ == '__main__':
-    device = torch.device('cuda')
+    parser = argparse.ArgumentParser(description='Protein Pretraining')
+
+    parser.add_argument('--device', default="cuda", type=str)
+    parser.add_argument('--data_path', default="data.json", type=str)
+    parser.add_argument('--batch_size', default=128, type=int)
+    parser.add_argument('--hidden_dim', default=32, type=int)
+    parser.add_argument('--num_layers', default=5, type=int)
+    parser.add_argument('--temperature', default=0.2, type=float)
+    parser.add_argument('--epochs', default=200, type=int, metavar='N',
+                        help='number of total epochs to run')
+    parser.add_argument('--lr', '--learning-rate', default=0.01, type=float)
+    parser.add_argument('--topk', default=(1,), type=tuple)
+
+    args = parser.parse_args("")
+    
     main()
